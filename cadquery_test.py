999675a719ab1a96f61711dff6789752e866a4c0
Ihor Proniuk <157399550+IhorProniuk@users.noreply.github.com>
2024-05-18 14:04:28 +0300

    Update cadquery_test.py

--- cadquery_test.py
+++ cadquery_test.py
@@ -1,6 +1,18 @@
 import cadquery as cq
-result = cq.Workplane("XY").box(90, 50, 20) \
-    .faces(">Z").workplane().hole(10)
+
+# Створюємо куб
+result = cq.Workplane("XY").box(90, 50, 20)
+
+# Додаємо отвори
+result = result.faces(">Z").workplane() \
+    .pushPoints([(20, 20), (70, 20), (20, 30), (70, 30)]) \
+    .hole(5) \
+    .faces(">Z").workplane() \
+    .pushPoints([(20, 40), (70, 40)]) \
+    .hole(7)
+
+# Згладжуємо кути
+result = result.edges("|Z").fillet(5)
 
 if __name__=='__main__':
     show_object(result)
